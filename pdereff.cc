4ab285ce02e6efaadc88c7a63e8bfd86dcee5e3c
karitra <karev.alexandre@gmail.com>
2017-02-16 13:33:03 +0300

    init commit

---
+++ pdereff.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+
+#include <string> 
+
+using namespace std;
+
+struct Foo {
+	
+	void inc(int to_add) { z += to_add; }
+	
+	int z;
+};
+
+struct A {
+
+	// not working A() : some_data{ new Foo() } {}
+	A() { 
+		some_data.uptr.reset(new Foo());
+	}
+
+
+	struct {
+		unique_ptr<Foo> uptr;
+	} some_data;
+}; 
+
+int main(int argc, char **argv)
+{
+	auto add = (argc > 1) ? stoi(argv[1]) : 1310;
+
+	A a;
+	auto i = long{};
+
+	auto &ref = a.some_data.uptr;
+	
+	// test st
+	i += 11;
+
+	a.some_data.uptr->inc(add);
+	ref->inc(add);
+ 
+	std::cerr << "z = " <<  ref->z << '\n';
+	std::cerr << "i = " <<  i << '\n';
+
+	return 0;
+}
+
