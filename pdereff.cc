0aa3abe8fd1861fecce518b1ebeac453fb619e90
karitra <karev.alexandre@gmail.com>
2017-02-16 14:46:36 +0300

    add conlusion and disass. results

--- pdereff.cc
+++ pdereff.cc
@@ -1,3 +1,6 @@
+//
+// Conclusion: complete elision of pointer dereferencing at GCC -O2 level 
+//
 #include <cstdlib>
 #include <memory>
 
@@ -7,7 +10,7 @@ struct Foo {
 	
 	void inc(int to_add) { z += to_add; }
 	
-	int z;
+	int z = 125;
 };
 
 struct A {
@@ -30,7 +33,16 @@ int main(int argc, char **argv)
 
 	auto &ref = a.some_data.uptr;
 
+	//
+	// Resulting assambler for following code:
+	// 
+   	//  4:	bb 1e 05 00 00       	mov    ebx,0x51e
+	// ...
+	// 1d:	8d 5c 5b 7d          	lea    ebx,[rbx+rbx*2+0x7d]
+	//
 	a.some_data.uptr->inc(add);
+	a.some_data.uptr->inc(add);
+	
 	ref->inc(add);
  
 	return a.some_data.uptr->z;
