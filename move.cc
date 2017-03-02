50cacee3e2a709992d2d20f5ac9645b45b433130
karitra <karev.alexandre@gmail.com>
2017-03-02 17:29:30 +0300

    move sample updated

--- move.cc
+++ move.cc
@@ -22,15 +22,31 @@ struct A {
 	int z;
 };
 
+struct B 
+{
+	B(A a) : local(std::move(a)) {}
+	A local;
+};
 
-void foo(A a) {
+int foo(A a) {
 	cerr << "foo: a.z = " << a.z << '\n';
+	return a.z;
+}
+
+B boo() {
+	A a(100500);
+	a.z++;
+
+	// return B(std::move(a));
+	return B(a);
 }
 
 int main()
 {
 	//A b(1310);
-	foo(A(100500));
-	//foo(move(b));
+	//foo(A(100500));
+	const auto b = boo();
+
+	cerr << "b.z " << b.local.z << '\n';
 }
 
